431a0469959479db1f39f417a56c5c4adecb151d
Emirrah Hazelle Dogelio <em.dogelio@teamcodeflux.com>
2015-06-18 10:04:10 +0800

    Implement input of the 3 colors to be randomized later
    Signed-off-by: Emirrah Hazelle Dogelio <em.dogelio@teamcodeflux.com>

--- src/main/java/com/emirrah/app/MainActivity.java
+++ src/main/java/com/emirrah/app/MainActivity.java
@@ -1,15 +1,33 @@
 package com.emirrah.app;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+import org.androidannotations.annotations.*;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+@EActivity(R.layout.main)
+public class MainActivity extends Activity {
+
+    @ViewById
+    EditText first_color;
+
+    @ViewById
+    EditText second_color;
+
+    @ViewById
+    EditText third_color;
+
+    @Click(R.id.submit_colors)
+    public void submitColors() {
+        String first = first_color.getText().toString();
+        String second = second_color.getText().toString();
+        String third = third_color.getText().toString();
+
+        String[] colors = { first, second, third };
+
+        for (int i = 0; i < colors.length; i += 1) {
+            Toast toast = Toast.makeText(this, colors[i], Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
